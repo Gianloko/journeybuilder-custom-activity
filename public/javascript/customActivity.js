7bd0800a56b9d32ff4e5ec21b803aea8bdc9d599
gtessitore <tex27491>
2022-01-05 14:14:32 +0100

    test MC

--- public/javascript/customActivity.js
+++ public/javascript/customActivity.js
@@ -109,7 +109,7 @@ function save() {
             {
                 "contactKey" : "{{Contact.Key}}",
 				"emailField" : "{{InteractionDefaults.Email}}",
-				"eventField" : "{{Contact.Attribute.PushNotificationTest.Event}}",
+				"eventField" : "{{Event.\"DEAudience-1bc977ef-f8f6-f7c1-4f5f-0ad296980e22\".Event}}",
 				"textField"  : "{{Contact.Attribute.\"PushNotificationTest\".\"Text\"}}",
 				"dateField"  : "{{Event.\"DEAudience-1bc977ef-f8f6-f7c1-4f5f-0ad296980e22\".\"Date\"}}"
             }
