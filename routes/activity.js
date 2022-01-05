472a2c515f6b84235b153a9371364d872583b509
gtessitore <tex27491>
2022-01-05 17:20:09 +0100

    test MC

--- routes/activity.js
+++ routes/activity.js
@@ -24,7 +24,7 @@ exports.execute = async (req, res) => {
 	
 	logger.info(id);
 	
-	SFClient.fetchPostData(data).then(res => logger.info(res.json())).catch(err => logger.info(err); logger.info(err.message));
+	SFClient.fetchPostData(data).then(res => logger.info(res.json())).catch(err => logger.info(err));
 	
 	logger.info("end of request");
 	
